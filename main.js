282c61e8cb6fe9b3bac0ffaa09a0995f1212be9d
alek <alekristeskii@gmail.com>
2022-06-30 13:14:08 +0200

    update

--- main.js
+++ main.js
@@ -2,21 +2,17 @@ const display = document.querySelector("[data-display]");
 const caclButtons = Array.from(document.querySelectorAll("button"));
 
 caclButtons.forEach((calc) => {
-    calc.addEventListener("click", (e) => {
-        if (e.target.classList.contains("equal")) {
-            try {
-                return (display.innerHTML = eval(display.innerHTML));
-            } catch {
-                return (display.innerHTML = "error");
-            }
-        }
-        if (display.innerHTML === "error") {
-            display.innerHTML = "";
-        }
-        display.innerHTML += e.target.innerHTML;
-    });
+  calc.addEventListener("click", (e) => {
+    if (e.target.classList.contains("equal")) {
+      try {
+        return (display.innerHTML = eval(display.innerHTML));
+      } catch {
+        return (display.innerHTML = "error");
+      }
+    }
+    if (display.innerHTML === "error") {
+      display.innerHTML = "";
+    }
+    display.innerHTML += e.target.innerHTML;
+  });
 });
-
-// keydown
-// interval
-// numbers
